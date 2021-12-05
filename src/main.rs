287218b34314ff62651cb29d69b96096a66aff1b
Your Name <you@example.com>
2021-12-05 11:28:08 +0900

    Add log for debug

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use study_google_auth::{configuration::get_configuration, startup::Application};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("[Study Google Identity] Starting main");
     let configuration = get_configuration();
     let application = Application::build(configuration).await?;
     println!("Starting app on :{}", application.port());
